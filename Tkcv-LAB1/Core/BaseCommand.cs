c04513f2aef6d9bf2a7ea10ec1f62cf46ee81c08
Stanislav Forester <baxtoban555308775@outlook.com>
2022-10-12 17:07:56 +0300

    add pagination and view product

---
+++ Tkcv-LAB1/Core/BaseCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Tkcv_LAB1.Core;
+
+public class BaseCommand : ICommand
+{
+    private readonly Action<object> _method;
+    private readonly Predicate<object> _canExecute;
+
+    public BaseCommand(Action<object> method)
+        : this(method, null)
+    {
+
+    }
+
+    public BaseCommand(Action<object> method, Predicate<object> canExecute)
+    {
+        _method = method;
+        _canExecute = canExecute;
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+        return _canExecute is null ? true : _canExecute(parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+        _method.Invoke(parameter);
+    }
+}
--- Tkcv-LAB1/MVVM/ViewModels/MainWindowVM.cs
+++ Tkcv-LAB1/MVVM/ViewModels/MainWindowVM.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using Tkcv_LAB1.Core;
 using Tkcv_LAB1.MVVM.Models;
 
@@ -11,49 +14,56 @@ public class MainWindowVM : ObservableObject
     private string _searchData;
     private string _selectedSort;
     private string _selectedFiltrationSetting;
+    private int _totalPage;
     private ObservableCollection<string> _sortTypes;
     private ObservableCollection<string> _filtrationSettings;
-    private ObservableCollection<Product> _products;
-
+    private ObservableCollection<Product> _allProducts;// all product
+    private ObservableCollection<Product> _products;// view data
+    private ObservableCollection<Button> _buttons;
+    
     public MainWindowVM()
     {
+        _totalPage = 1;
         _sortTypes = new ObservableCollection<string>();
         _filtrationSettings = new ObservableCollection<string>();
         _products = new ObservableCollection<Product>();
+        _buttons = new ObservableCollection<Button>();
+        _allProducts = new ObservableCollection<Product>();
         
+        InitButtons();
+
         // for test, then del 
         SortTypes.Add("Alphabet (a-Z)");
         SortTypes.Add("Type");
         _filtrationSettings.Add("Low price to upper");
         _filtrationSettings.Add("Upper price to lower");
+        AddTestData(25);
+        WriteViewList(0, 4);
+    }
+
+    public void GetPressedButtonNumber(object sender)
+    {
+        var content = sender.ToString();
+        var result = int.TryParse(content, out var newNumberPage);
         
-        Products.Add(new Product()
+        if (result is false)
         {
-            Name = "Name of product",
-            Price = 2300m,
-            VendorCode = "220-444",
-            Materials = new List<string>()
-            {
-                "mat1",
-                "mat2",
-                "mat3"
-            },
-            Type = "Type of product",
-            PathImg = "D:\\meme\\280fbcdca7af8211808471ecaf067654.jpg"
-        });
-        Products.Add(new Product()
+            ChangeTotalPageByStep(content);
+        }
+        else
         {
-            Name = "jopaPis",
-            Price = 22300m,
-            VendorCode = "550-831",
-            Materials = new List<string>()
-            {
-                "mat1",
-                "mat2",
-            },
-            Type = "Tyf produt2",
-            PathImg = "D:\\meme\\280fbcdca7af8211808471ecaf067654.jpg"
-        });
+            if(_totalPage == newNumberPage) return;
+            _totalPage = newNumberPage;
+        }
+        
+        var to = _totalPage * 4;
+        var from = to - 4;
+        
+        if (to > _allProducts.Count) to = _allProducts.Count;
+        if(from >= _allProducts.Count || from < 0) return;
+
+        WriteViewList(from, to);
+        ChangeButtons(_totalPage);
     }
 
     public string SearchData
@@ -90,5 +100,94 @@ public class MainWindowVM : ObservableObject
     {
         get => _products;
         set => Set(ref _products, value, nameof(Products));
+    }
+
+    public ObservableCollection<Button> Buttons
+    {
+        get => _buttons;
+        set => Set(ref _buttons, value, nameof(Buttons));
+    }
+
+    public ObservableCollection<Product> AllProducts
+    {
+        get => _allProducts;
+        set => Set(ref _allProducts, value, nameof(AllProducts));
+    }
+
+    // test method [DELETE THIS]
+    private void AddTestData(int count)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            AllProducts.Add(new Product()
+            {
+                Name = $"Name of product{i}",
+                Price = new Random().Next(100, 20000),
+                VendorCode = "220-444",
+                Materials = new List<string>()
+                {
+                    "mat1",
+                    "mat2",
+                    "mat3"
+                },
+                Type = "Type of product",
+                PathImg = "D:\\meme\\280fbcdca7af8211808471ecaf067654.jpg"
+            });
+        }
+    }
+
+    private void InitButtons()
+    {
+        var btn = new string[] {"-", "1", "2", "3", "4", "5", "+" };
+
+        _buttons.Clear();
+        foreach (var item in btn)
+        {
+            _buttons.Add(new Button()
+            {
+                Content = item,
+                Command = new BaseCommand(GetPressedButtonNumber),
+                CommandParameter = item,
+                Width = 20
+            });
+        }
+    }
+
+    private void ChangeButtons(int clickedNumber)
+    {
+        if(clickedNumber * 4 > _allProducts.Count) return; 
+
+        if (clickedNumber <= 3)
+        {
+            InitButtons();
+            return;
+        }
+
+        var length = _buttons.Count;
+        var leftCount = length / 2 - 1;
+        var first = clickedNumber - leftCount;
+        
+        for (var i = 1; i < length - 1; i++)
+        {
+            _buttons[i].Content = first;
+            _buttons[i].CommandParameter = first;
+            first++;
+        }
+    }
+
+    private void WriteViewList(int from, int to)
+    {
+        // write view list
+        _products.Clear();
+        for (var i = from; i < to; i++)
+        {
+            _products.Add(_allProducts[i]);
+        }
+    }
+
+    private void ChangeTotalPageByStep(string content)
+    {
+        if (content == "+") _totalPage++;
+        else if (content == "-" && _totalPage > 1) _totalPage--;
     }
 }
